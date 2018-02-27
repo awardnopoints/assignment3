26d2d48d47e5195aabe0ff7850a3178855e8456e
eoin <awardnopoints@gmail.com>
2018-02-27 12:58:31 +0000

    built class

---
+++ ledCheck/main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb 27 09:36:01 2018
+
+@author: eoin
+"""
+
+import Light
+
+test = Light.Light(5)
+print(test.grid)
---
+++ test/tests.py
@@ -0,0 +1,22 @@
+import unittest
+
+from ..ledCheck.Light import Light
+
+#class TestLights(unittest.TestCase):
+    
+#    def test_size(self, n):
+#        test = Light(n)
+#        count = 0
+#        for i in test:
+#            for j in i:
+#                count += 1
+#        self.assertEqual(count, n**2)
+#    
+#    def test_sizes(self):
+#        test_size(5)
+#        test_size(20)
+#        test_size(1000)
+#        test_size(0)
+
+if __name__ == '__main__':
+    unittest.main()
