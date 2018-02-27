3c6b94e4cfe4a87e26736e7f64715fdac302fac3
eoin <awardnopoints@gmail.com>
2018-02-27 17:24:12 +0000

    Filled out main.py, implemented regex. Still need to figure out how to take input. Untested, need to make better tests.

--- test/tests.py
+++ test/tests.py
@@ -11,10 +11,6 @@ class TestLights(unittest.TestCase):
         self.assertEqual(test_grids(100), 100**2)
         self.assertEqual(test_grids(1), 1)
         self.assertEqual(test_grids(0), 0)
-        self.assertEqual(test_grids(5), 5)
-        self.assertEqual(test_grids(5), 5)
-        
-
         
     def wrong_test(self):
         self.assertEqual(test_grids(5), 5)
