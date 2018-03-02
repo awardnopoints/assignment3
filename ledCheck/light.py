7732c2fd2dc128bba550648e86297447e19ae516
eoin <awardnopoints@gmail.com>
2018-03-02 16:11:28 +0000

    adjusted parameters of grid search

--- ledCheck/light.py
+++ ledCheck/light.py
@@ -8,30 +8,60 @@ class Light:
     
     def on(self, x1, y1, x2, y2):
         a, b, x, y = min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)
-        for i in range(a, x+1):
-            for j in range(b, y+1):
+        i = max(a, 0)
+        j = max(b, 0)
+        while i <= x and i < self.side:
+            while j <= y and j < self.side:
                 if isinstance(self.grid[i][j], int):
                     self.unresolvedCount -= 1
                     if self.grid[i][j] % 2 == 0:
                         self.onFinal += 1
                     self.grid[i][j] = None
+                j += 1
+            i += 1
+#        for i in range(a, x+1):
+#            for j in range(b, y+1):
+#                if isinstance(self.grid[i][j], int):
+#                    self.unresolvedCount -= 1
+#                    if self.grid[i][j] % 2 == 0:
+#                        self.onFinal += 1
+#                    self.grid[i][j] = None
         
     def off(self, x1, y1, x2, y2):
         a, b, x, y = min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)
-        for i in range(a, x+1):
-            for j in range(b, y+1):
+        i = max(a, 0)
+        j = max(b, 0)
+        while i <= x and i < self.side:
+            while j <= y and j < self.side:
                 if isinstance(self.grid[i][j], int):
                     self.unresolvedCount -= 1
                     if self.grid[i][j] % 2 == 1:
                         self.onFinal += 1
                     self.grid[i][j] = None
+                j += 1
+            i += 1
+#        for i in range(a, x+1):
+#            for j in range(b, y+1):
+#                if isinstance(self.grid[i][j], int):
+#                    self.unresolvedCount -= 1
+#                    if self.grid[i][j] % 2 == 1:
+#                        self.onFinal += 1
+#                    self.grid[i][j] = None
     
     def switch(self, x1, y1, x2, y2):
         a, b, x, y = min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)
-        for i in range(a, x+1):
-            for j in range(b, y+1):
+        i = max(a, 0)
+        j = max(b, 0)
+        while i <= x and i < self.side:
+            while j <= y and j < self.side:
                 if isinstance(self.grid[i][j], int):
                     self.grid[i][j] += 1
+                j += 1
+            i += 1
+#        for i in range(a, x+1):
+#            for j in range(b, y+1):
+#                if isinstance(self.grid[i][j], int):
+#                    self.grid[i][j] += 1
     
     def resolveSwitches(self):
         for i in range(self.side):
@@ -39,4 +69,3 @@ class Light:
                 if isinstance(self.grid[i][j], int):
                     if self.grid[i][j] % 2 == 1:
                         self.onFinal += 1
-
