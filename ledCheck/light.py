8eea485cf86fd27b7f62f25803371b1ab80cce1a
eoin <awardnopoints@gmail.com>
2018-03-02 17:00:09 +0000

    Fixed parameters

--- ledCheck/light.py
+++ ledCheck/light.py
@@ -6,24 +6,33 @@ class Light:
     
     def on(self, x1, y1, x2, y2):
         a, b, x, y = min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)
-        for i in range(a, x+1):
-            for j in range(b, y+1):
-                if i < self.side and j < self.side:
-                    self.grid[i][j] = True
+        x = min(x, self.side-1)
+        y = min(y, self.side-1)
+        i = max(a, 0)
+        while i <= x:
+            j = max(b, 0)
+            while j <= y:
+                self.grid[i][j] = True
         
     def off(self, x1, y1, x2, y2):
         a, b, x, y = min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)
-        for i in range(a, x+1):
-            for j in range(b, y+1):
-                if i < self.side and j < self.side:
-                    self.grid[i][j] = False
+        x = min(x, self.side-1)
+        y = min(y, self.side-1)
+        i = max(a, 0)
+        while i <= x:
+            j = max(b, 0)
+            while j <= y:
+                self.grid[i][j] = False
     
     def switch(self, x1, y1, x2, y2):
         a, b, x, y = min(x1, x2), min(y1, y2), max(x1, x2), max(y1, y2)
-        for i in range(a, x+1):
-            for j in range(b, y+1):
-                if i < self.side and j < self.side:
-                    self.grid[i][j] = not self.grid[i][j]
+        x = min(x, self.side-1)
+        y = min(y, self.side-1)
+        i = max(a, 0)
+        while i <= x:
+            j = max(b, 0)
+            while j <= y:
+                self.grid[i][j] = not self.grid[i][j]
     
     def count(self):
         tally = 0
